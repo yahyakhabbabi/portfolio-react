cceabd71133bbfb9e3858bb96e0c4a4e7165bc11
yahya <khabbabiyahia@gmail.com>
2023-12-27 01:29:51 +0100

    m

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,35 +2,48 @@ import React from 'react';
 import './Portfolio.css';
 import store from '../../assests/store.png';
 import dashboard from '../../assests/dashboard.png';
-import facture from '../../assests/facture.jpg';
-import store2 from '../../assests/store2.avif'
+
+
+const projects = [
+  {
+    image: store,
+    title: 'Store Project',
+    description: 'An e-commerce store project showcasing various products.',
+    reviewLink: 'https://example.com/store-review',
+    codeLink: 'https://github.com/example/store',
+  },
+  {
+    image: dashboard,
+    title: 'Dashboard Project',
+    description: 'A data visualization dashboard for analytics purposes.',
+    reviewLink: 'https://example.com/dashboard-review',
+    codeLink: 'https://github.com/example/dashboard',
+  },
+  // Add more project objects here...
+];
 
 export default function Portfolio() {
-  // Array of image URLs from Unsplash as placeholders
-  const imageArray = [
-    store,
-    store2,
-    dashboard,
-    store2,
-  ];
-
   return (
     <section id="works">
-<div>
-  <h2 className='workstitle'>My Portfolio</h2>
-  <span className='workDesc'>
-    Welcome to my portfolio! Here, I've gathered a collection of my work that represents my dedication and passion. Each project reflects not only my skills but also my commitment to delivering excellence. I've poured my heart and soul into these endeavors, constantly pushing the boundaries to achieve more. This portfolio is a testament to my growth, and I'm thrilled to share it with you. Thank you for taking the time to explore and appreciate the effort behind each creation.
-  </span>
-</div>
+      <div>
+        <h2 className='workstitle'>My Portfolio</h2>
+        <span className='workDesc'>
+          Welcome to my portfolio! Here, I've gathered a collection of my work that represents my dedication and passion. Each project reflects not only my skills but also my commitment to delivering excellence. I've poured my heart and soul into these endeavors, constantly pushing the boundaries to achieve more. This portfolio is a testament to my growth, and I'm thrilled to share it with you. Thank you for taking the time to explore and appreciate the effort behind each creation.
+        </span>
+      </div>
       <div className='worksImgs'>
-        {imageArray.map((imageUrl, index) => (
-          <div key={index} className='workContainer'>
+        {projects.map((project, index) => (
+          <div key={index} className={`workContainer ${index % 2 === 0 ? 'workEnterFromLeft' : 'workEnterFromRight'}`}>
             <div className='imageWrapper'>
-              <img src={imageUrl} alt={`Work ${index}`} className='workImg'/>
+              <img src={project.image} alt={`Work ${index}`} className='workImg'/>
               <div className='overlay'>
-                <button className='workBtn'>Review</button>
-                <button className='workBtn'>Code</button>
+                <a href={project.reviewLink} target='_blank' rel='noopener noreferrer' className='workBtn'>Review</a>
+                <a href={project.codeLink} target='_blank' rel='noopener noreferrer' className='workBtn'>Code</a>
               </div>
+            </div>
+            <div className='projectInfo'>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
             </div>
           </div>
         ))}
